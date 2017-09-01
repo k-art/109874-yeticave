ffd10b61a3e43b7d2136da093dbeca29c0c6f565
Artem Kozlov <arte.horn@gmail.com>
2017-09-01 12:16:42 +0200

    add security

--- templates/index.php
+++ templates/index.php
@@ -42,15 +42,15 @@
 
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$value['url']; ?>" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=htmlspecialchars($value['url']); ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=$value['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['name']; ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($value['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=htmlspecialchars($value['price']); ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
