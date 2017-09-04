ad55265a46f2faaf64384b51cc04a4ec65712ee1
Artem Kozlov <arte.horn@gmail.com>
2017-09-04 09:29:46 +0200

    do query string

--- templates/index.php
+++ templates/index.php
@@ -45,7 +45,7 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=$value['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="<?='lot.php?id=' . current($lots);?>"><?=htmlspecialchars($value['name']); ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="<?='lot.php?id=' . $key;?>"><?=htmlspecialchars($value['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
