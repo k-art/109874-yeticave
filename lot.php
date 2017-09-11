c9724ab60226bec77119546ecdf0a5fd2f7d0d8b
Artem Kozlov <arte.horn@gmail.com>
2017-09-12 01:25:28 +0200

    remove unnecessary code

--- lot.php
+++ lot.php
@@ -3,10 +3,6 @@ require_once ('functions.php');
 require_once ('lots_data.php');
 
 $title = 'Лот';
-
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
-$is_auth = (bool) rand(0, 1);
 $id = '';
 
 $lot_data = [
@@ -21,9 +17,6 @@ $content = render_template('lot', $lot_data);
 $layout_data = [
     'title' => $title,
     'categories' => $categories,
-    'user_name' => $user_name,
-    'user_avatar' => $user_avatar,
-    'is_auth' => $is_auth,
     'content' => $content
 ];
 
