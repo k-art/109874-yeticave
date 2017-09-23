a637b580c062a2751cf21813d9afcf99f6980abc
Artem Kozlov <arte.horn@gmail.com>
2017-09-23 23:09:45 +0200

    connect to MySQL (fix)

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-require_once ('functions.php');
 require_once ('lots_data.php');
+require_once ('functions.php');
+require_once ('init.php');
 
 $title = 'Главная';
 
--- init.php
+++ init.php
@@ -1,25 +1,19 @@
 <?php
-require_once ('functions.php');
-require_once ('lots_data.php');
-
-
+require_once ('config.php');
 $error = '';
 
-$connect = mysqli_connect("localhost", "root", "", "yeticave");
+$connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
 
 if (!$connect) {
     $error = mysqli_connect_error();
     $content = render_template('error', ['categories' => $categories, 'error' => $error]);
-}
-else {
-    $content = '<h2>Соединение установлено</h2>';
-}
 
-$layout_data = [
-    'title' => 'Проверка соединения',
-    'categories' => $categories,
-    'content' => $content
-];
+    $layout_template = render_template('layout', [
+        'title' => 'Проверка соединения',
+        'categories' => $categories,
+        'content' => $content
+    ]);
 
-$layout_template = render_template('layout', $layout_data);
 print ($layout_template);
+exit();
+}
